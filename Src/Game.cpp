c541d76c87e2c907e0ff356a5ed296c120d50d62
Reece <Reece2.Bellchambers@live.uwe.ac.uk>
2024-10-02 19:38:17 +0100

    Update function in game class has comments in the code.

--- Src/Game.cpp
+++ Src/Game.cpp
@@ -2,9 +2,7 @@
 
 // Constructor
 Game::Game() :
-	window(window = new sf::RenderWindow(sf::VideoMode(1920, 1080), "SFML works!")), event(event)
-{
-}
+	window(window = new sf::RenderWindow(sf::VideoMode(1920, 1080), "SFML works!")), event(event){}
 
 // Destructor
 Game::~Game()
@@ -23,7 +21,6 @@ void Game::Run()
 		HandleInputs();
 		Render();
 	}
-
 }
 
 // Intialize any data needed for game objects
@@ -39,49 +36,63 @@ void Game::Update()
 {
 	if (currentScene == GAMEPLAY)
 	{
+		// Stops both paddles from going off the window screen
 		player1.ConstrainPaddle();
 		player2.ConstrainPaddle();
+
+		// Ball movements updated each frame
 		ball.Move(*window);
 
+		// Checks collision between player 1 and ball
 		if (ball.BallBounds().intersects(player1.PaddleBounds()))
 		{
+			// Find the centers of player 1 paddle and ball
 			float paddleCenter = player1.GetPosition().y + player1.GetScale().y / 2.0f;
 			float ballCenter = ball.GetPosition().y + ball.GetScale().y;
 
+			// Find the difference of the centers and normalize
 			float difference = ballCenter - paddleCenter;
-
 			float normalizedDifference = difference / (player1.GetScale().x / 2.0f);
 
+			// if the ball hits the top of paddle, ball will go upwards
 			if (normalizedDifference >= -7.5f && normalizedDifference < 24.5)
 			{
 				ball.SetVelocity(1.0f, -0.4f);
 			}
+			// if the ball hits the middle of paddle ball
 			else if (normalizedDifference >= 24.5 && normalizedDifference < 34.5)
 			{
 				ball.SetVelocity(1.0f, -0.1f);
 			}
+			// if the ball hits the bottom of the paddle, ball goes downwards
 			else if (normalizedDifference >= 34.5 && normalizedDifference <= 63)
 			{
 				ball.SetVelocity(1.0f, 0.4f);
 			}
 		}
 
+		// Checks collision between player 2 and ball
 		if (ball.BallBounds().intersects(player2.PaddleBounds()))
 		{
+			// Find the centers of player 2 paddle and ball
 			float paddleCenter = player2.GetPosition().y + player2.GetScale().y / 2.0f;
 			float ballCenter = ball.GetPosition().y + ball.GetScale().y;
 
+			// Find the difference of the centers and normalize
 			float difference = ballCenter - paddleCenter;
 			float normalizedDifference = difference / (player2.GetScale().x / 2.0f);
 
+			// if the ball hits the top of paddle, ball will go upwards
 			if (normalizedDifference >= -7.5f && normalizedDifference < 24.5)
 			{
 				ball.SetVelocity(-1.0f, -0.4f);
 			}
+			// if the ball hits the middle of paddle ball
 			else if (normalizedDifference >= 24.5 && normalizedDifference < 34.5)
 			{
 				ball.SetVelocity(-1.0f, -0.1f);
 			}
+			// if the ball hits the bottom of the paddle, ball goes downwards
 			else if (normalizedDifference >= 34.5 && normalizedDifference <= 63)
 			{
 				ball.SetVelocity(-1.0f, 0.4f);
